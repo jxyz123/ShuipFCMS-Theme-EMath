eddfc0bb57dedea5c49847d35dd54644415a829d
jxyz123 <510735632@qq.com>
2015-06-21 22:03:33 +0800

    修改首页及教师列表页
    修改首页footer时间，修改图片链接、更新友情链接，将教师列表页“发布时间”改为“更新时间”

--- shuipf/Template/EMath/Content/links.php
+++ shuipf/Template/EMath/Content/links.php
@@ -2,24 +2,26 @@
 <!--links-->
 <div class="link-box pull-left">
     <div class="link-img mg-b-10">
+        <a href="http://maths.hust.edu.cn/index.php?a=shows&catid=10&id=1018"><img src="{$config_siteurl}statics/EMath/images/link_recruitment_talents.jpg"></a>
+    </div>
+    <div class="link-img mg-b-10">
         <a href="http://maths.hust.edu.cn/mate/index_cn.php"><img src="{$config_siteurl}statics/EMath/images/link_schoolfellow.jpg"></a>
     </div>
     <div class="link-img mg-b-10">
-        <a href="http://maths.hust.edu.cn/yysx"><img src="{$config_siteurl}statics/EMath/images/link_math_magazine.jpg"></a>
-    </div>
-    <div class="link-img mg-b-10">
-        <a href="http://maths.hust.edu.cn/new/index.asp"><img src="{$config_siteurl}statics/EMath/images/link_oldsite.jpg"></a>
+        <a href="http://yysx.hust.edu.cn/"><img src="{$config_siteurl}statics/EMath/images/link_math_magazine.jpg"></a>
     </div>
     <div class="link-img link-form">
     <div class="">
         <form>
             <select onChange="if(this.options[this.selectedIndex].value!=''){window.open(this.options[this.selectedIndex].value,'_blank');}">
-            <option value="1">&lt; Friendly Links &gt;</option>
-            <option value="http://www.hust.edu.cn">华中科技大学</option>
-            <option value="http://hub.hust.edu.cn">华中科技大学HUB系统</option>
-            <option value="http://student.hust.edu.cn">学工在线</option>
+            <option value="http://maths.hust.edu.cn/">&lt; Friendly Links &gt;</option>
+            <option value="http://maths.hust.edu.cn/new/index.asp">数学与统计学院旧版网站入口</option>
+            <option value="http://www.hust.edu.cn/">华中科技大学</option>
+            <option value="http://hub.hust.edu.cn/">华中科技大学HUB系统</option>
             <option value="http://math.hust.edu.cn/gksx/">工科数学教学基地</option>
-            <option value="http://blog.sina.com.cn/u/2483597433">华中科技大学数学学科服务</option>
+            <option value="http://mathcenter.hust.edu.cn/">华中科技大学数学中心</option>
+            <option value="http://shumo.hust.edu.cn/">HUST数模科技创新基地</option>
+            <option value="http://blog.sina.com.cn/u/2483597433/">华中科技大学数学学科服务</option>
             <option value="http://hust.so/">华科搜—华科专属上网首页</option>
             </select>
         </form>
